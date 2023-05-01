1a534e32ace461dcce14f7c3c2fbd0cc18df361e
Shreshth Tripathi <Shreshth542002@gmail.com>
2023-05-01 13:24:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState(null)
+  const [phoneNumber, setPhoneNumber] = useState('');
+
+  const handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    const response = await axios.get(`https://basicdeployment.onrender.com/${phoneNumber}`);
+    setData(response.data);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+    setData(null);
+  }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+          <input type="text" onChange={(e) => setPhoneNumber(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+      {data ? (
+        <div>
+          <p>Name: {data[0].name}</p>
+          <p>Device Id : {data[0].device_id}</p>
+        </div>
+      ): (
+        <p>No data to display.</p>
+      )}
+    </>
   );
 }
 
